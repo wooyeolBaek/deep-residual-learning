814aeef3afe2bec21733d22be125a15cdc904423
Wooyeol Baek <wooyeolbaek@Wooyeols-MacBook-Pro.local>
2023-03-31 22:01:45 +0900

    style: Change variable names

--- blocks.py
+++ blocks.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 
-class ConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True) -> torch.FloatTensor:
+class ConvBN(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super().__init__()
 
         self.layer = nn.Sequential(
@@ -12,7 +12,6 @@ class ConvLayer(nn.Module):
                 kernel_size=kernel_size,
                 stride=stride,
                 padding=padding,
-                bias=bias,
             ),
             nn.BatchNorm2d(
                 num_features=out_channels,
@@ -24,32 +23,30 @@ class ConvLayer(nn.Module):
 
 
 class PlainBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, bias=True) -> torch.FloatTensor:
+    def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        self.h = nn.Sequential(
-            ConvLayer(
+        self.f = nn.Sequential(
+            ConvBN(
                 in_channels=in_channels,
                 out_channels=out_channels,
                 kernel_size=3,
                 stride=1 if in_channels==out_channels else 2, # 2 for downsampling
                 padding=1,
-                bias=bias,
             ),
             nn.ReLU(),
-            ConvLayer(
+            ConvBN(
                 in_channels=out_channels,
                 out_channels=out_channels,
                 kernel_size=3,
                 stride=1,
                 padding=1,
-                bias=bias,
             ),
         )
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.h(x)
+        x = self.f(x)
         x = self.relu(x)
 
         return x
@@ -63,9 +60,9 @@ class IdentityMap(nn.Module):
         
         # identity mapping with zero padding: in_channels!=out_channels
         if in_channels != out_channels:
-            channels_num = (out_channels - in_channels)
-            front_pad_num = channels_num//2
-            back_pad_num = channels_num - front_pad_num
+            nchannels = (out_channels - in_channels)
+            nfront_pad = nchannels//2
+            nback_pad = nchannels - nfront_pad
 
             self.shortcut = nn.Sequential(
                 nn.MaxPool2d(
@@ -74,14 +71,14 @@ class IdentityMap(nn.Module):
                     padding=0,
                 ),
                 nn.ConstantPad3d(
-                    padding=(0,0,0,0,front_pad_num,back_pad_num),
+                    padding=(0,0,0,0,nfront_pad,nback_pad),
                     value=0,
                 )
             )
     
     def forward(self, x):
         # identity mapping with zero padding: in_channels!=out_channels
-        if not self.shortcut is None:
+        if self.shortcut is not None:
             return self.shortcut(x)
         
         # identity mapping: in_channels==out_channels
@@ -89,14 +86,14 @@ class IdentityMap(nn.Module):
 
 
 class ProjectionMap(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, stride=2):
         super().__init__()
 
         self.shortcut = nn.Conv2d(
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=1,
-            stride=2
+            stride=stride,
         )
     
     def forward(self, x):
@@ -105,8 +102,8 @@ class ProjectionMap(nn.Module):
 
 
 class ResBlock(PlainBlock):
-    def __init__(self, in_channels, out_channels, bias=True, mapping='identity') -> torch.FloatTensor:
-        super().__init__(in_channels, out_channels, bias)
+    def __init__(self, in_channels, out_channels, mapping='A'):
+        super().__init__(in_channels, out_channels)
         
         # option A: Identity Mapping with Zero Padding
         self.shortcut = IdentityMap(
@@ -114,116 +111,129 @@ class ResBlock(PlainBlock):
             out_channels=out_channels,
         )
 
-        # option B: Projection Mapping
-        if in_channels!=out_channels and mapping=='projection':
+        # option B: Identity Mapping with Projection Mapping
+        # option C: Projection Mapping
+        if (in_channels!=out_channels and mapping=='B') or mapping=='C':
             self.shortcut = ProjectionMap(
                 in_channels=in_channels,
                 out_channels=out_channels,
+                stride=2 if in_channels!=out_channels else 1 # 1 for C
             )
     
     def forward(self, x):
-        x = self.h(x) + self.shortcut(x)
+        x = self.f(x) + self.shortcut(x)
         x = self.relu(x)
 
         return x
 
 
 class BottleneckBlock(ResBlock):
-    def __init__(self, in_channels, out_channels, bias=True, mapping='identity') -> torch.FloatTensor:
-        super().__init__(in_channels, out_channels, bias, mapping)
+    def __init__(self, in_channels, out_channels, mapping='A'):
+        super().__init__(in_channels, out_channels, mapping)
 
-        self.h = nn.Sequential(
-            ConvLayer(
+        self.f = nn.Sequential(
+            ConvBN(
                 in_channels=in_channels,
                 out_channels=out_channels,
                 kernel_size=1,
                 stride=1,
                 padding=0,
-                bias=bias,
             ),
             nn.ReLU(),
-            ConvLayer(
+            ConvBN(
                 in_channels=out_channels,
                 out_channels=out_channels,
                 kernel_size=3,
                 stride=1 if in_channels==out_channels else 2, # 2 for downsampling
                 padding=1,
-                bias=bias,
             ),
             nn.ReLU(),
-            ConvLayer(
+            ConvBN(
                 in_channels=out_channels,
                 out_channels=out_channels,
                 kernel_size=1,
                 stride=1,
                 padding=0,
-                bias=bias,
             ),
         )
 
 
 if __name__=="__main__":
 
-    batch_size = 16
-    in_channels = 64
+    batch_size = 4
+    in_channels = 16
     out_channels = 128
     size = 32
+    size=224
 
-    # input: (16, 64, 32, 32)
+    # input: (4, 16, 32, 32)
     x = torch.randn((batch_size,in_channels,size,size))
 
-    # ConvLayer Test
-    print('ConvLayer Test')
+    # ConvBN Test
+    print('ConvBN Test')
     # the same dimension
-    conv_out64 = ConvLayer(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1, bias=True)(x)
-    # dimension expansion
-    conv_out128 = ConvLayer(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=True)(x)
+    conv_out16 = ConvBN(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1)(x)
+    # downsampling: dimension expansion
+    conv_out32 = ConvBN(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)(x)
 
-    print(f'conv_out64 Output, shape: {tuple(conv_out64.shape)} == (16,64,32,32)')
-    print(f'conv_out128 Output, shape: {tuple(conv_out128.shape)} == (16,128,32,32)')
+    print(f'conv_out64 Output, shape: {tuple(conv_out16.shape)} == (4,16,32,32)')
+    print(f'conv_out32 Output, shape: {tuple(conv_out32.shape)} == (4,32,16,16)')
 
-    
+    # conv1
+    print('Conv 1 Test')
+    # imagenet
+    #conv_out1 = ConvBN(in_channels=in_channels, out_channels=in_channels, kernel_size=7, stride=2, padding=3)(x)
+    #print(f'conv_out1 Output, shape: {tuple(conv_out1.shape)} == (4,32,16,16)')
+    conv_out1 = ConvBN(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1)(x)
+    print(f'conv_out1 Output, shape: {tuple(conv_out1.shape)} == (4,32,16,16)')
+
     # PlainBlock Test
     print('PlainBlock Test')
     # the same dimension
-    plain_out64 = PlainBlock(in_channels=in_channels, out_channels=in_channels, bias=True)(x)
+    plain_out16 = PlainBlock(in_channels=in_channels, out_channels=in_channels)(x)
     # dimension expansion
-    plain_out128 = PlainBlock(in_channels=in_channels, out_channels=out_channels, bias=True)(x)
+    plain_out32 = PlainBlock(in_channels=in_channels, out_channels=out_channels)(x)
 
-    print(f'plain_out64 Output, shape: {tuple(plain_out64.shape)} == (16,64,32,32)')
-    print(f'plain_out128 Output, shape: {tuple(plain_out128.shape)} == (16,128,16,16)')
+    print(f'plain_out16 Output, shape: {tuple(plain_out16.shape)} == (4,16,32,32)')
+    print(f'plain_out32 Output, shape: {tuple(plain_out32.shape)} == (4,32,16,16)')
     
 
     # ResBlock Test
     print('ResBlock Test')
     # the same dimension identity
-    res_out64_iden = ResBlock(in_channels=in_channels, out_channels=in_channels, bias=True, mapping='identity')(x)
+    res_out16_iden = ResBlock(in_channels=in_channels, out_channels=in_channels, mapping='A')(x)
     # dimension expansion identity
-    res_out128_iden = ResBlock(in_channels=in_channels, out_channels=out_channels, bias=True, mapping='identity')(x)
+    res_out32_iden = ResBlock(in_channels=in_channels, out_channels=out_channels, mapping='A')(x)
     # the same dimension projection
-    res_out64_proj = ResBlock(in_channels=in_channels, out_channels=in_channels, bias=True, mapping='projection')(x)
+    res_out16_proj = ResBlock(in_channels=in_channels, out_channels=in_channels, mapping='B')(x)
     # dimension expansion projection
-    res_out128_proj = ResBlock(in_channels=in_channels, out_channels=out_channels, bias=True, mapping='projection')(x)
+    res_out32_proj = ResBlock(in_channels=in_channels, out_channels=out_channels, mapping='B')(x)
+    # the same dimension projection
+    res_out16_iden_proj = ResBlock(in_channels=in_channels, out_channels=in_channels, mapping='C')(x)
+    # dimension expansion projection
+    res_out32_iden_proj = ResBlock(in_channels=in_channels, out_channels=out_channels, mapping='C')(x)
 
-    print(f'res_out64_identity Output, shape: {tuple(res_out64_iden.shape)} == (16,64,32,32)')
-    print(f'res_out128_identity Output, shape: {tuple(res_out128_iden.shape)} == (16,128,16,16)')
-    print(f'res_out64_projection Output, shape: {tuple(res_out64_proj.shape)} == (16,64,32,32)')
-    print(f'res_out128_projection Output, shape: {tuple(res_out128_proj.shape)} == (16,128,16,16)')
+    print(f'res_out16_identity Output, shape: {tuple(res_out16_iden.shape)} == (4,16,32,32)')
+    print(f'res_out32_identity Output, shape: {tuple(res_out32_iden.shape)} == (4,32,16,16)')
+    print(f'res_out16_projection Output, shape: {tuple(res_out16_proj.shape)} == (4,16,32,32)')
+    print(f'res_out32_projection Output, shape: {tuple(res_out32_proj.shape)} == (4,32,16,16)')
+    print(f'res_out16_projection Output, shape: {tuple(res_out16_iden_proj.shape)} == (4,16,32,32)')
+    print(f'res_out32_projection Output, shape: {tuple(res_out32_iden_proj.shape)} == (4,32,16,16)')
 
 
     # BottleneckBlock Test
     print('BottleneckBlock Test')
     # the same dimension identity
-    bottleneck_out64_iden = BottleneckBlock(in_channels=in_channels, out_channels=in_channels, bias=True, mapping='identity')(x)
+    bottleneck_out16_iden = BottleneckBlock(in_channels=in_channels, out_channels=in_channels, mapping='A')(x)
     # dimension expansion identity
-    bottleneck_out128_iden = BottleneckBlock(in_channels=in_channels, out_channels=out_channels, bias=True, mapping='identity')(x)
+    bottleneck_out32_iden = BottleneckBlock(in_channels=in_channels, out_channels=out_channels, mapping='A')(x)
     # the same dimension projection
-    bottleneck_out64_proj = BottleneckBlock(in_channels=in_channels, out_channels=in_channels, bias=True, mapping='projection')(x)
+    bottleneck_out16_proj = BottleneckBlock(in_channels=in_channels, out_channels=in_channels, mapping='B')(x)
     # dimension expansion projection
-    bottleneck_out128_proj = BottleneckBlock(in_channels=in_channels, out_channels=out_channels, bias=True, mapping='projection')(x)
+    bottleneck_out32_proj = BottleneckBlock(in_channels=in_channels, out_channels=out_channels, mapping='B')(x)
 
-    print(f'bottleneck_out64_identity Output, shape: {tuple(bottleneck_out64_iden.shape)} == (16,64,32,32)')
-    print(f'bottleneck_out128_identity Output, shape: {tuple(bottleneck_out128_iden.shape)} == (16,128,16,16)')
-    print(f'bottleneck_out64_projection Output, shape: {tuple(bottleneck_out64_proj.shape)} == (16,64,32,32)')
-    print(f'bottleneck_out128_projection Output, shape: {tuple(bottleneck_out128_proj.shape)} == (16,128,16,16)')
+    print(f'bottleneck_out16_identity Output, shape: {tuple(bottleneck_out16_iden.shape)} == (4,16,32,32)')
+    print(f'bottleneck_out32_identity Output, shape: {tuple(bottleneck_out32_iden.shape)} == (4,32,16,16)')
+    print(f'bottleneck_out16_projection Output, shape: {tuple(bottleneck_out16_proj.shape)} == (4,16,32,32)')
+    print(f'bottleneck_out32_projection Output, shape: {tuple(bottleneck_out32_proj.shape)} == (4,32,16,16)')
 
